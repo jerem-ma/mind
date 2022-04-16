013d1a9a53230c01e7b11af9bdf09ee5f5914006
Jérémy M <jeremy.maia@protonmail.com>
2022-04-16 15:21:58 +0200

    Add prototypes to lists.h

--- includes/lists.h
+++ includes/lists.h
@@ -6,7 +6,7 @@
 /*   By: jmaia <jmaia@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/16 14:56:42 by jmaia             #+#    #+#             */
-/*   Updated: 2022/04/16 14:57:23 by jmaia            ###   ########.fr       */
+/*   Updated: 2022/04/16 15:21:42 by jmaia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,8 @@ typedef struct s_list
 	void			*content;
 	struct s_list	*next;
 }	t_list;
+
+void	ft_lstadd_front(t_list	**lst, t_list *new);
+t_list	*ft_lstnew(void *content);
 
 #endif
